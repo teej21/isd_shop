781fef784b91825782ae098033a2317294cdea4d
Teej <vinhnek117@gmail.com>
2024-04-22 21:56:02 +0700

    config cors

--- src/main/java/org/isd/shop/configurations/WebSecurityConfig.java
+++ src/main/java/org/isd/shop/configurations/WebSecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,10 +27,7 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-
-                .cors()
-                .and()
+        http.cors(c -> c.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(request -> {
@@ -52,6 +54,21 @@ public class WebSecurityConfig {
                         })
                 );
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
--- src/main/java/org/isd/shop/responses/common/ErrorResultResponse.java
+++ src/main/java/org/isd/shop/responses/common/ErrorResultResponse.java
@@ -2,9 +2,17 @@ package org.isd.shop.responses.common;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
 public class ErrorResultResponse {
     @JsonProperty("error")
     private String error;
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"error\":\"" + error + '\"' +
+                '}';
+    }
 }
